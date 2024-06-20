34c3ca11b2038408f911e4cdf1b676b3f24d66f2
leoarnauld <ljoutsamembou@gmail.com>
2024-06-20 13:39:27 +0200

    fonctionalité detail user

--- gestion-utilisateurs/src/app/users/user-details/user-details.component.ts
+++ gestion-utilisateurs/src/app/users/user-details/user-details.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
-export class UserDetailsComponent {
 
+export class UserDetailsComponent implements OnInit {
+  user: User | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.userService.getUserById(id).subscribe(data => {
+        this.user = data;
+      });
+    }
+  }
+
+  editUser(): void {
+    if (this.user) {
+      this.router.navigate(['/edit-user', this.user.id]);
+    }
+  }
+
+  deleteUser(): void {
+    if (this.user && confirm('Voulez-vous vraiment supprimer cet utilisateur ?')) {
+      this.userService.deleteUser(this.user.id).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    }
+  }
 }
